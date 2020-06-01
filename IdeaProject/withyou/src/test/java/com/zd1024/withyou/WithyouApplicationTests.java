fdfa5dba20d6f732f0da19e0a22bfef27d79e629
Albert9527 <55827613+Albert9527@users.noreply.github.com>
2020-06-01 09:46:28 +0800

    静态资源

--- IdeaProject/withyou/src/test/java/com/zd1024/withyou/WithyouApplicationTests.java
+++ IdeaProject/withyou/src/test/java/com/zd1024/withyou/WithyouApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.UUID;
 
 @SpringBootTest
 class WithyouApplicationTests {
@@ -27,8 +28,16 @@ class SampleTest {
     public void testSelect() {
         System.out.println(("----- selectAll method test ------"));
         List<User> userList = userMapper.selectList(null);
-       /* Assert.assertEquals(5, userList.size());*/
+        /* Assert.assertEquals(5, userList.size());*/
         userList.forEach(System.out::println);
+    }
+
+    @Test
+    public void test() {
+        String u1 = UUID.randomUUID().toString();
+        String u2 = UUID.randomUUID().toString();
+        System.out.println("》》》》》"+u1);
+        System.out.println("》》》》》"+u2);
     }
 
 }
